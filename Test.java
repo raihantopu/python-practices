87c958a8302cb9f379fbe2a90f491fa5309b8c78
raihantopu <topuraihan85@gmail.com>
2024-12-11 12:40:51 +0600

    changs in Test.java

--- Test.java
+++ Test.java
@@ -2,36 +2,26 @@ import java.util.*;
 
 public class Test{
     public static void main(String[] args){
-
-        String s = "07:05:45PM";
-        System.out.println(solve(s));
+        System.out.println(solve(Arrays.asList(1,2,1,3,2), 3, 2));
     }
 
-    private static String solve(String s){
-        if(s.contains("- 12:00:00AM")){
-            return "00:00:00";
-        }
-        if(s.contains("- 12:00:00PM")){
-            return "12:00:00";
-        }
-        if(s.contains("PM")){
-            String subString = s.substring(0, s.length() - 2);
-            String[] splitedString = subString.split(":");
-            if(Integer.valueOf(splitedString[0]) == 12){
-                return splitedString[0] + ":" + splitedString[1] + ":" + splitedString[2];
-            }else{
-               int hour = Integer.valueOf(splitedString[0]) + 12;
-                return hour + ":" + splitedString[1] + ":" + splitedString[2]; 
+    private static int solve(List<Integer> s, int d, int m){
+        int count = 0;
+        for(int i=0;i<s.size();i++){
+            int sum = 0;
+            int barsCount = 0;
+            for(int j=i;j<j+m;j++){
+                if(j>=s.size() || sum >= d){
+                    break;
+                }
+                barsCount++;
+                sum+=s.get(j);
             }
-        }else{
-            Stripng subString = s.substring(0, s.length() - 2);
-            String[] splitedString = subString.split(":");
-            if(Integer.valueOf(splitedString[0]) == 12){
-                return "00:" + splitedString[1] + ":" + splitedString[2]; 
-            }else{
-                return s.substring(0, s.length() - 2);
+            if(sum == d && barsCount == m){
+                count++;
             }
-           
         }
+        return count;
     }
+
 }
