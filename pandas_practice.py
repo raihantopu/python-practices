a9893972ea3d3f57daa762e89de74fd3e722e0e4
raihantopu <topuraihan85@gmail.com>
2024-12-19 16:27:55 +0600

    slide changes in practices

--- pandas_practice.py
+++ pandas_practice.py
@@ -1,17 +1,46 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-import random
+from sklearn.model_selection import StratifiedShuffleSplit
 
-data = pd.read_csv('titanic.csv')
-# data['Survived'].value_counts().plot.pie(autopct='%1.1f%%', startangle=90, cmap='coolwarm', legend=True)
+# Load the data
+data = pd.read_csv('data/train.csv')
+
+# Stratified split
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2)
+for train_indices, test_indices in split.split(data, data["Survived"]):  # Use "Survived" for stratification
+    strat_train_set = data.loc[train_indices]
+    strat_test_set = data.loc[test_indices]
+
+# Plot histograms
+plt.figure(figsize=(12, 6))
+
+# Training set histograms
+plt.subplot(1, 2, 1)
+strat_train_set['Survived'].hist(alpha=0.7, label="Survived", color="blue")
+strat_train_set['Pclass'].hist(alpha=0.7, label="Pclass", color="green")
+plt.title("Training Set")
+plt.legend()
+
+# Test set histograms
+plt.subplot(1, 2, 2)
+strat_test_set['Survived'].hist(alpha=0.7, label="Survived", color="blue")
+strat_test_set['Pclass'].hist(alpha=0.7, label="Pclass", color="green")
+plt.title("Test Set")
+plt.legend()
+
+plt.show()
+
+
+
+
+#data['Survived'].value_counts().plot.pie(autopct='%1.1f%%', startangle=90, cmap='coolwarm', legend=True)
 
 # # Customize the pie chart
 # plt.title("Survival Distribution")
 # plt.ylabel("")  # Hide the ylabel for a cleaner look
 # plt.show()
 
-print(data[['Sex', 'Survived']].groupby('Sex')['Survived'].mean())
+#print(data[['Sex', 'Survived']].groupby('Sex')['Survived'].mean())
 
 
 # xpoints = np.random.randint(10, 1000, size=20)
